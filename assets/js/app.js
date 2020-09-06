d160afb10f50b354c933b680b163211a31759108
EJBaquet <ejbaquet@yahoo.com>
2020-09-06 18:36:57 -0400

    add svg and parms

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 10,
+  right: 40,
+  bottom: 60,
+  left: 100,
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - marging.top - margin.bottom;
+
+// Create SVG warpper; append SVG group
+var svg = d3
+  .select(".scatter")
+  .append(svg)
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);

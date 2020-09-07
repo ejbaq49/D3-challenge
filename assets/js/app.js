c616b4da13ea3309262dcbdb0f48ff69f64e0548
EJBaquet <ejbaquet@yahoo.com>
2020-09-07 00:47:24 -0400

    add bubble plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,8 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create SVG warpper; append SVG group
 var svg = d3
-  .select(".scatter")
-  .append(svg)
+  .select("#scatter")
+  .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
@@ -25,8 +25,63 @@ var chartGroup = svg
 
 // Read Data
 d3.csv("../assets/data/data.csv")
-  .then(function (data) {
-    console.log(data);
+  .then(function (stateData) {
+    // print data
+    console.log(stateData);
+
+    // data conversion
+    stateData.forEach((data) => {
+      data.poverty = +data.poverty;
+      data.povertyMoe = +data.povertyMoe;
+      data.age = +data.age;
+      data.ageMoe = +data.ageMoe;
+      data.income = +data.income;
+      data.incomeMoe = +data.incomeMoe;
+      data.healthcare = +data.healthcare;
+      data.healthcareLow = +data.healthcareLow;
+      data.healthcareHigh = +data.healthcareHigh;
+      data.obesity = +data.obesity;
+      data.obesityLow = +data.obesityLow;
+      data.obesityHigh = +data.obesityHigh;
+      data.smokes = +data.smokes;
+      data.smokesLow = +data.smokesLow;
+      data.smokesHigh = +data.smokesHigh;
+    });
+
+    // create scale functions for Poverty and Healthcare
+    var xLinearScale = d3
+      .scaleLinear()
+      .domain([8, d3.max(stateData, (d) => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3
+      .scaleLinear()
+      .domain([2, d3.max(stateData, (d) => d.healthcare)])
+      .range([height, 0]);
+
+    // axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append axes to chart
+    chartGroup
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g").call(leftAxis);
+
+    // Create Circles
+    var circlesGroup = chartGroup
+      .selectAll("circle")
+      .data(stateData)
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => xLinearScale(d.poverty))
+      .attr("cy", (d) => yLinearScale(d.healthcare))
+      .attr("r", "15")
+      .attr("fill", "pink")
+      .attr("opacity", ".5");
   })
   .catch(function (error) {
     console.log(error);
